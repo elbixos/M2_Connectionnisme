3c882212b9d1e4e5a32d8953c8bc3111de850a92
elbixos <vpage.vincent@gmail.com>
2019-11-14 21:48:30 -0400

    saving every 100 epochs

--- DeepMim/Sources/basicRegression.py
+++ DeepMim/Sources/basicRegression.py
@@ -49,7 +49,7 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 # Create a callback that saves the model's weights
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  save_weights_only=True,
-                                                 verbose=1,save_frequency=100)
+                                                 verbose=1,save_freq=14300)
 
 
 
