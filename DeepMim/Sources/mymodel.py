2acbe1cb33ec41872a5668001da2a0ea83da577b
Vincent Page <vpage.vincent@gmail.com>
2019-11-15 13:08:51 -0400

    build_model as function

--- DeepMim/Sources/mymodel.py
+++ DeepMim/Sources/mymodel.py
@@ -3,17 +3,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 from tensorflow.keras import layers
 
-class Model():
-    def build_model():
-        model = keras.Sequential([
-          layers.Dense(32, activation='relu', input_shape=[4,]),
-          layers.Dense(32, activation='relu'),
-          layers.Dense(1)
-        ])
+def build_model():
+    model = keras.Sequential([
+      layers.Dense(32, activation='relu', input_shape=[4,]),
+      layers.Dense(32, activation='relu'),
+      layers.Dense(1)
+    ])
 
-        optimizer = tf.keras.optimizers.RMSprop(0.001)
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
 
-        model.compile(loss='mse',
-                      optimizer=optimizer,
-                      metrics=['mae', 'mse'])
-        return model
+    model.compile(loss='mse',
+                  optimizer=optimizer,
+                  metrics=['mae', 'mse'])
+    return model
